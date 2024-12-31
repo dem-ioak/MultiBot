b1b04d4efc312bf7a254c89dee09858b9a44d04b
Demetri Ioakimidis <dem.ioak@gmail.com>
2024-12-30 22:23:28 -0500

    Depreceated feature loading

--- bot/cogs/boards.py
+++ bot/cogs/boards.py
@@ -3,7 +3,7 @@ from discord.ext import commands, tasks
 from discord import app_commands, Embed, Color
 
 from util.buttons.board_buttons import BoardView, BoardListView
-from util.constants import BOARDS
+from util.constants import BOARDS, DEPRECEATED
 import util.dataclasses as DataClasses
 from util.helper_functions import board_view_description
 
@@ -17,12 +17,13 @@ class Boards(commands.Cog):
     @app_commands.command(name = "boards", description = "View and select a board for this server")
     @app_commands.checks.cooldown(1, 120)
     async def board_view(self, interaction : discord.Interaction):
-        guild_id = interaction.guild.id
-        embed = Embed(title = "Server Boards", color = Color.red())
-        embed.description = board_view_description(guild_id, 1)
-        view = BoardListView(guild_id, interaction.user.id, interaction.message)
-        await interaction.response.send_message(embed = embed, view = view)
-        view.message = await interaction.original_response()
+        await interaction.response.send_message(embed = DEPRECEATED, ephemeral=True)
+        # guild_id = interaction.guild.id
+        # embed = Embed(title = "Server Boards", color = Color.red())
+        # embed.description = board_view_description(guild_id, 1)
+        # view = BoardListView(guild_id, interaction.user.id, interaction.message)
+        # await interaction.response.send_message(embed = embed, view = view)
+        # view.message = await interaction.original_response()
 
 async def setup(client):
     await client.add_cog(Boards(client))
